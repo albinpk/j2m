ef6155e63fc368df7eeecc818e9b349ee3f83ee2
Albin <56157868+albinpk@users.noreply.github.com>
2025-03-08 22:25:21 +0530

    build: web

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2468398366"
+    serviceWorkerVersion: "2306093809"
   }
 });
