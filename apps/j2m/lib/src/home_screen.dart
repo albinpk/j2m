be1705e15c0e108e1e1a80d8d22a1c69e76bf45a
Albin <56157868+albinpk@users.noreply.github.com>
2025-03-11 23:03:27 +0530

    refactor: update initial json data

--- apps/j2m/lib/src/home_screen.dart
+++ apps/j2m/lib/src/home_screen.dart
@@ -33,8 +33,10 @@ class _HomeScreenState extends State<HomeScreen> {
   "id": 1,
   "name": "John Doe",
   "email": "johndoe@example.com",
-  "age": 30,
-  "isActive": true
+  "profile": {
+    "age": 30,
+    "isActive": true
+  }
 }
 ''',
     language: json,
